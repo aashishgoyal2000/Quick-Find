47bb42d3ffce151be08806df2b410a5efed12f8f
Aashish Goyal <54860971+aashishgoyal2000@users.noreply.github.com>
2020-04-20 13:51:46 +0530

    Weighted Quick Union Implementation

---
+++ Weighted Quick Union Algo/WeightedQuickUnion.java
@@ -0,0 +1,37 @@
+class WeightedQuickUnion{
+	int arr[],connectedComponents[];
+
+	WeightedQuickUnion(int n){
+		arr = new int[n];
+		connectedComponents = new int[n];
+		for(int i=0;i<n;i++){
+			arr[i] = i;
+			connectedComponents[i] = 1;
+		}
+	}
+	
+	void union(int p,int q){
+		int pid = findRoot(p);
+		int qid = findRoot(q);
+		if(pid == qid)return;
+		if( connectedComponents[pid] > connectedComponents[qid] ){
+			arr[qid] = arr[pid];
+			connectedComponents[pid] += connectedComponents[qid];
+		}
+		else{
+			arr[pid] = arr[qid];
+			connectedComponents[qid] += connectedComponents[pid];
+		}
+	}
+	
+	boolean connected(int p,int q){
+		return findRoot(p) == findRoot(q);
+	}
+	
+	private int findRoot(int p){
+		while(p!=arr[p]){
+			p = arr[p];
+		}
+		return p;
+	}
+}
